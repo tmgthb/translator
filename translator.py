a848910594b9fb3d14bd5e3a908e55af970e3015
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2022-05-12 23:53:04 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -5,4 +5,4 @@ openai.api_key = st.secrets["SECRET_KEY"]
 st.title('Traductor: español-finés')
 prompt_text = st.text_input(label="Escribir el texto en español:")
 response = openai.Edit.create(engine="text-davinci-edit-001", input="prompt_text", instruction="Translate to English", temperature=0.3, top_p=1)
-st.text(response["choices"][0]["text"])
+st.text(response)
