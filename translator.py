3eea0a1f885f3efef6f770df4bf5384531fedd1c
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2022-05-12 23:59:41 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,7 +2,7 @@ import openai
 import os
 import streamlit as st
 openai.api_key = st.secrets["SECRET_KEY"]
-st.title('Traductor: español-finés')
-prompt_text = st.text_input(label="Escribir el texto en español:")
+st.title('Traductor:')
+prompt_text = st.text_input(label="español-finés")
 response = openai.Edit.create(engine="text-davinci-edit-001", input=prompt_text, instruction="Translate to Finnish", temperature=0.3, top_p=1)
 st.text(response["choices"][0]["text"])
